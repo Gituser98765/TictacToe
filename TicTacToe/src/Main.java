ab99d0f4920a9268ea9c027d954e6908b9b04366
Gituser98765 <josefine@LAPTOP-4EM6LU1B.local>
2018-08-17 20:35:15 +0200

    gitlab

--- TicTacToe/src/Main.java
+++ TicTacToe/src/Main.java
@@ -15,11 +15,7 @@ public class Main {
 		// TODO Auto-generated method stub
 		
 		
-		System.out.println("hello");
-		System.out.println("hello");
-		
-		System.out.println("hello");
-		System.out.println("hello");
+		System.out.println("hihi");
  
 	}
 
